68c41bcc86488f305b0870b9d7262945737c2c05
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-12-01 10:04:52 +0000

    Config file for Superclustering sample dumper

---
+++ runs/AlessandroElectronConfigs/supercls_sampleDump_fromRECO.py
@@ -0,0 +1,49 @@
+# Runs SuperclusteringSampleDumper from a step3 file 
+
+import FWCore.ParameterSet.Config as cms
+
+
+from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
+
+process = cms.Process('RECOScDump',Phase2C17I13M9)
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:step3.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+# Output
+process.TFileService = cms.Service("TFileService",
+                                    fileName=cms.string("superclsDump.root")
+                                    )
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step3b nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads = 20
+process.options.numberOfStreams = 0
+
+
+process.dumper = cms.EDAnalyzer("SuperclusteringSampleDumper",
+)
+
+process.p = cms.EndPath(process.dumper)
+
+
+process.Timing = cms.Service("Timing",
+  summaryOnly = cms.untracked.bool(False),
+  useJobReport = cms.untracked.bool(True)
+)
