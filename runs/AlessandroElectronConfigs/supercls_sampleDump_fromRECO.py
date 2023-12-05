59b8d257f13fc2ac743f7383a20f63d948dfa76d
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-12-05 13:26:46 +0000

    Fix superlcs_sampleDump config

--- runs/AlessandroElectronConfigs/supercls_sampleDump_fromRECO.py
+++ runs/AlessandroElectronConfigs/supercls_sampleDump_fromRECO.py
@@ -11,7 +11,7 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
 )
 
 # Input source
