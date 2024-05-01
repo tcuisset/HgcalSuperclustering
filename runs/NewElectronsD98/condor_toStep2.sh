e5d0e2ddc624219f0dd7d98af8b8f5f0fd5b593d
Théo Cuisset <theo.cuisset@polytechnique.edu>
2024-05-01 11:44:25 +0200

    New electrons configs step1-2

---
+++ runs/NewElectronsD98/condor_toStep2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+export X509_USER_PROXY=/grid_mnt/vol_home/llr/cms/cuisset/.t3/proxy.cert
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /grid_mnt/vol_home/llr/cms/cuisset/hgcal/supercls/repoForJobs/CMSSW_14_1_X_2024-04-29-1100
+cmsenv
+cd - 
+
+cat  <<EOF >>SingleElectron.py
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper 
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService) 
+randSvc.populate() 
+EOF
+cmsRun SingleElectron.py
+
+cat  <<EOF >>step2.py
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper 
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService) 
+randSvc.populate() 
+EOF
+cmsRun step2.py
+
+mkdir -p /grid_mnt/data_cms_upgrade/cuisset/supercls/NewElectronsD98/$VERSION
+mv step2.root /grid_mnt/data_cms_upgrade/cuisset/supercls/NewElectronsD98/$VERSION/step2_$INDEX.root
